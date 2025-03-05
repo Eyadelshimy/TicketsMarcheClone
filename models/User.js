6a8ddc3de239c9047bcad50a383140da9fb6035a
Eyadelshimy <eyad.elshimy@student.giu-uni.de>
2025-03-05 21:29:07 +0200

    reformating all the codes

--- models/User.js
+++ models/User.js
@@ -2,14 +2,20 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true, index: true },
-        password: { type: String, required: true },
-        profilePicture: { type: String, required: true },
-        role: { type: String, enum: ["User", "Organizer", "Admin"], required: true },
+            name: { type: String, required: true },
+            email: { type: String, required: true, unique: true, index: true },
+            password: { type: String, required: true },
+            profilePicture: { type: String, required: true },
+            role: {
+                    type: String,
+                    enum: ["User", "Organizer", "Admin"],
+                    required: true
+            }
     },
-    { timestamps: true,strict: "throw" }
+    {
+            timestamps: true,
+            strict: "throw",
+    }
 );
 
-const User = mongoose.model("User", userSchema);
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
