35e7d80ab3b4f27015166bcd9ade4df742d8d6fd
Eyadelshimy <eyad.elshimy@student.giu-uni.de>
2025-03-05 21:25:24 +0200

    reformating the code

--- models/Booking.js
+++ models/Booking.js
@@ -1,31 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Schema = mongoose.Schema;
-
-const bookingSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+const bookingSchema = new mongoose.Schema(
+    {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        event: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
+        numberOfTickets: { type: Number, required: true },
+        totalPrice: { type: Number, required: true },
+        status: {
+            type: String,
+            enum: ["pending", "confirmed", "canceled"],
+            required: true
+        }
     },
-    event: {
-        type: Schema.Types.ObjectId,
-        ref: 'Event',
-        required: true
-    },
-    numberOfTickets:{
-        type: Number,
-        required: true
-    },
-    totalPrice:{
-        type: Number,
-        required: true
-    },
-    status:{
-        type: String,
-        enum: ['pending', 'confirmed', 'canceled'],
-        required: true
+    {
+        timestamps: true,
+        strict: "throw",
     }
-}, { timestamps: true, strict : "throw" });
+);
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
