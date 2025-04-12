34f7ca46f21cae7a7b5a76d2a1fb9c22c8516f84
Yassin Diab <yassodiab12345@gmail.com>
2025-04-12 21:08:58 +0200

    wallahi ana faqadt el amal fel denya

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,32 +1,34 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const User = require("../models/User");
 
-// Middleware to protect routes
-exports.protect = async (req, res, next) => {
-  let token;
+exports.roleProtect = (roles) => {
+  return async (req, res, next) => {
+    var token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+    if (req.cookies["token"]) {
+      token = req.cookies["token"];
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Token does not exist" });
+    }
 
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "Not authorized, no token",
-    });
-  }
+    var decoded;
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      res.status(401).json({ success: false, message: "Invalid token" });
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id);
-    next();
-  } catch (error) {
-    res.status(401).json({
-      success: false,
-      message: "Not authorized, token failed",
-    });
-  }
+    try {
+      req.user = await User.findOne({ userID: decoded.userID });
+      if (
+        roles.some((role) => role.toLowerCase() === req.user.role.toLowerCase())
+      ) {
+        next();
+      }
+    } catch (error) {
+      res.status(401).json({ success: false, message: "no." });
+    }
+  };
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
 const router = express.Router();
+const { roleProtect } = require("../middleware/auth");
 
 router.post("/login", login);
 router.post("/register", register);
