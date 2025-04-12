1bf262d013002ad4f53e48d409f51eca5e5b3235
kanzyhany <kanzyhany06@gmail.com>
2025-04-12 22:11:50 +0200

    admin controller and admin v1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRoutes");
+const adminRouter = require("./routes/adminRoutes");
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1", authRouter);
+app.use("/api/v1/admin", adminRouter);
 
 module.exports = app;
