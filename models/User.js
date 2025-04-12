a550505ed98260325342a3d0629c93e78f3057a2
Yassin Diab <yassodiab12345@gmail.com>
2025-04-12 19:32:52 +0200

    update models

--- models/User.js
+++ models/User.js
@@ -1,21 +1,30 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const userSchema = new mongoose.Schema(
-    {
-            name: { type: String, required: true },
-            email: { type: String, required: true, unique: true, index: true },
-            password: { type: String, required: true },
-            profilePicture: { type: String, required: true },
-            role: {
-                    type: String,
-                    enum: ["User", "Organizer", "Admin"],
-                    required: true
-            }
+  {
+    userID: { type: String, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    profilePicture: { type: String },
+    role: {
+      type: String,
+      enum: ["User", "Organizer", "Admin"],
+      required: true,
     },
-    {
-            timestamps: true,
-            strict: "throw",
-    }
+  },
+  {
+    timestamps: true,
+    strict: "throw",
+  },
 );
+
+userSchema.pre("save", function (next) {
+  if (!this.userID) {
+    this.userID = uuidv4();
+  }
+  next();
+});
 
 module.exports = mongoose.model("User", userSchema);
