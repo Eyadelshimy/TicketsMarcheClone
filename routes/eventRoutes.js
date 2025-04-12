5a9438dbf73e9885af0906de2f6c7e7c164f3435
Yassin Amr <yassinamr.m.06@gmail.com>
2025-04-12 22:25:29 +0200

    event shit

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllEvents, getEventById } = require('../controllers/eventsController');
 const { getOneEvent } = require('../eventController');
-<<<<<<< Updated upstream
-=======
-const {roleProtect} = require('../middleware/roleProtect');
->>>>>>> Stashed changes
 
 // List all events
 router.get('/', getAllEvents);
@@ -13,13 +9,4 @@ router.get('/', getAllEvents);
 // Get one event by title
 router.get('/:title', getOneEvent);
 
-<<<<<<< Updated upstream
-=======
-//get details of a single user
-router.get('/', getUserDetails);
-
-// Only admin can access
-router.get('/', roleProtect(['admin']), getAllEventsWithUserDetails); 
-
->>>>>>> Stashed changes
 module.exports = router;
