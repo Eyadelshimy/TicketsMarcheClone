c756549adb2e0020126cb6cf44dbb6d7c40888dd
Eyadelshimy <eyad.elshimy@student.giu-uni.de>
2025-04-14 18:37:03 +0200

    z

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/authRoutes");
+const eventRouter = require("./routes/eventRoutes");
 
 require("./models/User");
 require("./models/Event");
@@ -12,7 +13,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-
+app.use("/api/v1/events", eventRouter);
 app.use("/api/v1", authRouter);
 app.use("/api/v1/users", require("./routes/userRoutes"));
 
