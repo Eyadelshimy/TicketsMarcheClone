3089f01bcd37c39673fb514000731a6ce0339c4d
Eyadelshimy <eyad.elshimy@student.giu-uni.de>
2025-04-14 18:59:03 +0200

    afcz nbohp

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,17 +3,53 @@ const Event = require("../models/Event");
 module.exports = {
   createEvent: async (req, res) => {
     try {
+
+      const {
+        title,
+        description,
+        date,
+        location,
+        category,
+        image,
+        ticketPricing,
+        totalTickets,
+        remainingTickets,
+        status,
+      } = req.body;
+
       // Validate required fields
-      const { name, date, location } = req.body;
-      if (!name || !date || !location) {
+      if (
+          !title ||
+          !description ||
+          !date ||
+          !location ||
+          !category ||
+          !ticketPricing ||
+          !totalTickets ||
+          !remainingTickets ||
+          !status
+      ) {
         return res.status(400).json({
           success: false,
-          error: "Missing required fields: name, date, or location",
+          error: "Missing required fields",
         });
       }
 
-      // Create the event
-      const event = await Event.create(req.body);
+      // Create the event with the organizer set to req.user._id
+      const event = await Event.create({
+        title,
+        description,
+        date,
+        location,
+        category,
+        image,
+        ticketPricing,
+        totalTickets,
+        remainingTickets,
+        status,
+        organizer: req.user._id,
+      });
+
       res.status(201).json({ success: true, data: event });
     } catch (error) {
       // Handle validation errors
@@ -24,9 +60,8 @@ module.exports = {
         });
       }
 
-      // Handle other errors
+      //server error
       res.status(500).json({ success: false, error: "Server error" });
     }
   },
 };
-
