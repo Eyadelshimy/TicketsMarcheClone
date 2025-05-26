4ca710cf77c07ffc2241e379e69ab3dfe74279b1
Yassin Diab <yassin.diab@student.giu-uni.de>
2025-05-26 05:03:06 +0300

    add distance between navbar and page

--- frontend/src/Components/Layout.jsx
+++ frontend/src/Components/Layout.jsx
@@ -1,19 +1,20 @@
 // src/Components/Layout.jsx
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
-import Footer from './Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout() {
-    return (
-        <div className="app-container">
-            <Navbar />
-            <main className="main-content">
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="app-container">
+      <Navbar />
+      <main className="main-content mt-4">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Layout;
+
